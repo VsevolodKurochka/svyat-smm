3ec547a98cd6a059266d8e960f5a085ad81ef27d
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-03 01:28:17 +0300

    Add jquery mask

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -11,40 +11,6 @@ $(document).ready(function(){
 		scroll($( $(this).attr('href') ), 1500);
 	});
 
-	// Collapse
+	$('input[name="phone"]').mask('000-000-00-00');
 
-		$(".js-collapse__group.active").children(".js-collapse__group-body").slideDown();
-
-		$('.js-collapse').on('click', '.js-collapse__group-header', function(){
-			var collapseInner = $(this).parents('.js-collapse').find('.js-collapse__group');
-
-			$(this)
-				.parent()
-				.toggleClass('active');
-
-			$(this)
-				.next()
-				.slideToggle('slow');
-
-			collapseInner
-				.not($(this).parent())
-				.removeClass('active');
-
-			collapseInner
-				.children('.js-collapse__group-body')
-				.not($(this).next())
-				.slideUp("slow");
-
-		});
-	// Tabs
-		$('[data-action="tab"]').click(function(){			
-			// Tab links toggle class
-				$(this).closest(".vtabs__list").children("li").removeClass('active');
-				$(this).parent().addClass('active');
-			// Show tab content
-				var tabTarget = $(this).attr('data-target');
-				$(tabTarget).fadeIn('slow');
-				$(".vtabs__content > div").not($(tabTarget)).fadeOut('slow');
-		});
-	// Develope
 });	
